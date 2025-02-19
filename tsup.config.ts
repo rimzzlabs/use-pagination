dc60a90b3dd2d3bbc4dff0084b6f1bdeef6ccfcd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2025-02-19 16:57:14 +0700

    fix(tsup): adjust config to latest version

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'tsup'
 
-const env = process.env.NODE_ENV
-
 export default defineConfig({
-  splitting: true,
-  sourcemap: env === 'prod', // source map is only available in prod
-  clean: true, // rimraf disr
-  dts: true, // generate dts file for main module
-  format: ['cjs', 'esm'], // generate cjs and esm files
-  minify: env === 'production',
-  bundle: env === 'production',
-  skipNodeModulesBundle: true,
-  entryPoints: ['src/index.ts'],
-  watch: env === 'development',
+  entry: ['src/index.ts'],
   target: 'es2020',
+  splitting: false,
+  sourcemap: true,
+  clean: true,
   outDir: 'dist',
-  entry: ['src/**/*.ts'],
+  dts: true,
 })
